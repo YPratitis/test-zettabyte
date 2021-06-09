ddbf2e69c7285d7c5ed7fcea972753ea0284c207
yohanes123 <yohanespratitis@gmail.com>
2021-06-09 08:24:33 +0700

    Answet Javascript test1

--- Challenge_3/Javascript/test1.js
+++ Challenge_3/Javascript/test1.js
@@ -4,6 +4,11 @@ const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function result(input) {
   // Your Code Here
+  let total = 0;
+  for (let i=0; i<input.length; i++) {
+    total += input[i];
+  }
+return total;
 }
 
 console.log(result(input));
